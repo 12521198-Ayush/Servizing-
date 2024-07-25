cb87e934753b6e34ad57f3fc82a88c3498d726ea
Ayush Sharma <72184607+12521198-Ayush@users.noreply.github.com>
2024-07-25 11:19:43 +0530

    first commit

---
+++ src/app/js-route/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import React from 'react'
+import DataTable from '../test-table/DataTable/index'
+const page = () => {
+  return (
+    
+        <DataTable/>
+    
+    
+  )
+}
+
+export default page
